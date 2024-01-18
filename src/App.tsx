5b1883f32e388d9127d7c775428367a8d2947616
zengchaoming <zengchaoming@kernelsoft.com>
2024-01-18 16:12:00 +0800

    feat: update Portal

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,5 @@
-import { useEffect, useRef, useState } from 'react';
-import Portal from './components/Portal';
-import { getScrollBarSize } from './utils/scrollBar';
-import { createFakeInputStdIn } from './components/FakeInput/createFakeInputStdIn';
-import FakeInput from './components/FakeInput';
-
-const scrollbarSize = getScrollBarSize();
-
-console.log(scrollbarSize);
-
-const App: React.FC = () => {
-  const [value, setValue] = useState('');
-  useEffect(() => {
-    const { focus, destroy } = createFakeInputStdIn({
-      onChange: (value) => {
-        setValue(value);
-      },
-    });
-
-    focus();
-
-    return () => {
-      destroy();
-    };
-  }, []);
+export default function App() {
   return (
-    <div>
-      <FakeInput.Search
-        value={value}
-        round
-        onChange={(val) => {
-          setValue(val);
-        }}
-      />
-      <input />
-    </div>
-  );
-};
-
-export default App;
+    <div>app</div>
+  )
+}
--- src/components/Portal/index.tsx
+++ src/components/Portal/index.tsx
@@ -1,15 +1,19 @@
 import { useLatest } from '@/hooks/useLatest';
+import { useUnMount } from '@/hooks/useUnMount';
 import { ReactNode, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 export interface PortalProps {
   getPopupContainer?: () => HTMLElement;
   children?: ReactNode;
+  onMount?: (container: HTMLElement) => void;
+  onUnMount?: (container: HTMLElement | null) => void;
 }
 
-const Portal = ({ getPopupContainer, children }: PortalProps) => {
+const Portal = ({ getPopupContainer, children, onMount, onUnMount }: PortalProps) => {
   const [container, setContainer] = useState<HTMLElement | null>(null);
   const containerRef = useLatest(container);
+  const onMountRef = useLatest(onMount)
 
   useEffect(() => {
     if (getPopupContainer) {
@@ -19,11 +23,22 @@ const Portal = ({ getPopupContainer, children }: PortalProps) => {
       }
     } else {
       if (containerRef.current !== document.body) {
-        containerRef.current = document.body;
         setContainer(document.body);
       }
     }
-  }, [getPopupContainer]);
+  });
+
+
+  useEffect(() => {
+    if (container) {
+      onMountRef.current?.(container);
+    }
+  }, [container])
+
+
+  useUnMount(() => {
+    onUnMount?.(container);
+  })
 
   if (!container) return null;
 
